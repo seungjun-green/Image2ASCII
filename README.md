af7702ff0ec112757ce9a8ab72a98870822dd9fa
seungjun-green <lsj3285007@gmail.com>
2023-11-12 18:49:40 +0900

    Made some improvments on script.js

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Image2ASCII
 
-Testing git
 https://seungjun-green.github.io/Image2ASCII/
--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-
-
-function testing() {
-    console.log("Yout button worked")
-}
-
 document.getElementById('imageUploader').addEventListener('change', function(event) {
     var output = document.getElementById('preview');
     output.src = URL.createObjectURL(event.target.files[0]);
@@ -13,9 +7,9 @@ document.getElementById('imageUploader').addEventListener('change', function(eve
     }
 });
 
-
-
 document.getElementById('convertToTensor').addEventListener('click', function() {
+
+
     // Ensure that TensorFlow.js is loaded
     if (!tf) {
         console.error('TensorFlow.js is not loaded!');
@@ -27,15 +21,143 @@ document.getElementById('convertToTensor').addEventListener('click', function() 
 
     // Make sure an image is uploaded
     if (!imageElement.src || imageElement.style.display === 'none') {
-        alert('Please upload an image first.');
+        alert('Please upload an image first. :)');
         return;
     }
 
+    document.getElementById('displayArea').innerText = 'Loading...';
     
     
+    // STEP1. Get image tensor from uploaded image: (width, height, 3)
+    imageTensor = getTensorFromImage(imageElement);
+
+    // STEP2. Convert image into gray scale: (width, height, 1)
+    gray_tensor = rgbToGrayscale(imageTensor);
+
+
+    const iterations=10;
+    const width = gray_tensor.shape[0];
+    const height = gray_tensor.shape[1];
+    const num_of_elements = width * height;
     
+    let gray_tensor_1D = tf.reshape(gray_tensor, [num_of_elements])
+    let gray_tensor_2D = tf.stack(Array(8).fill(gray_tensor_1D), 1);
+    
+    let centroid_dict = {0: [], 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[]};
+    let centroid_dict_res = {0: [], 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[]};
+
+    // Only for the first time, randomaly initallize location of each centroid.
+    let centroids_1D = tf.randomUniform([8], 0, 256); 
+
+    // STEP3
+    // REPEAT:
+    //        assign centroid for all elements
+    //        update location of centroids
+    for (let iteration = 0; iteration < iterations; iteration++) {
+
+
+        //Step3-1: assign centroid for all elements(widt*hieght number of elements)
+        // get distance vector shape of (num_of_elements, 8) and get indicesOfMinValues array from
+        // it by selecting the column index which has the smallest value.
+        // as a result we're assigining centroid for all elements
+        let centroids_2D = tf.stack(Array(num_of_elements).fill(centroids_1D), 0);
+        let difference = tf.sub(gray_tensor_2D, centroids_2D);
+        let distance = tf.square(difference);
+        let indicesOfMinValues = Array.from(tf.argMin(distance, 1).dataSync());  
+        
+        
+        // STEP3-2: update location of centroids
+        // based on current centroids, update the each centroid's location as mean of its elements
+        // Update centroid_dict and centroid_dict_res dict.
+        // centroid_dict; each value: 0 ~ 256
+        // centroid_dict_res; each value: 0 ~ num_of_elements
+        for (let i = 0; i < num_of_elements; i++) {
+            centroid_dict[indicesOfMinValues[i]].push(gray_tensor_1D[i]);
+            centroid_dict_res[indicesOfMinValues[i]].push(i);
+        }
+        
+        let centroids_1D_new = [0, 0, 0, 0, 0, 0, 0, 0];
+        
+        // create an array containing new location of each centroid
+        for (let key in centroid_dict) {
+            if (centroid_dict[key].length > 0) {
+                centroids_1D_new[key] = tf.mean(centroid_dict[key])
+            }  
+        }
+        
+        // update centroid's locations.
+        centroids_1D = centroids_1D_new;
+    }
+    
+
+    // STEP6. Construcy output_string from result array.
+    final_string = constructFinalString(centroid_dict_res, width, height);
+
+    document.getElementById('displayArea').innerText = final_string;
+            
+});
+
+
+function constructFinalString(centroid_dict_res, width, height) {
+    let result = new Array(width * height).fill(0);
+    
+    Object.keys(centroid_dict_res).forEach(key => {
+        centroid_dict_res[key].forEach(element => {
+            result[element] = parseInt(key); 
+        });
+    });
+    // now each element in result array shows what centroid it belongs to    
+        
+    const charWeights = {
+        0: '-',
+        1: '=',
+        2: ':',
+        3: '+',
+        4: '*',
+        5: '%',
+        6: '#',
+        7: '@'
+    };
+    
+    
+    let final_string = ""
+    
+    // creating multi-line string(100 num of characters) * multi-lines
+    for (let i = 0; i < result.length; i++) {
+        final_string += charWeights[result[i]];
+        if ((i+1)%100 == 0) {
+            final_string+='\n';
+        }
+    }
+
+    return final_string
+}
+
+
+function reshapeTensor2(tensor) {
+    const height = tensor.shape[1];
+    const width = tensor.shape[0];
+    const reshapedTensor =  tensor.reshape([width, height]);
+
+    return reshapedTensor
+};
+
+function rgbToGrayscale(imageTensor) {
+    const [red, green, blue] = tf.split(imageTensor, 3, 2);
+
+    const R_WEIGHT = 0.299;
+    const G_WEIGHT = 0.587;
+    const B_WEIGHT = 0.114;
+
+    // Compute the grayscale values
+    const grayscale = red.mul(R_WEIGHT).add(green.mul(G_WEIGHT)).add(blue.mul(B_WEIGHT))
+    
+    return reshapeTensor2(grayscale);
+};
+
+
+function getTensorFromImage(imageElement) {
     // Calculate new dimensions
-    
     let originalWidth = imageElement.naturalWidth;
     let originalHeight = imageElement.naturalHeight;
     let newWidth = 100;
@@ -52,128 +174,7 @@ document.getElementById('convertToTensor').addEventListener('click', function() 
 
     // Convert the canvas to a tensor
     let tensor = tf.browser.fromPixels(canvas).toFloat()
-       
-
-    reshapedTensor = reshapeTensor(tensor);
-
-    gray_tensor = rgbToGrayscale(reshapedTensor);
-
-
-    let centroids_1D = tf.randomUniform([8], 0, 256); 
-
-
-    const iterations=5;
-    const width = gray_tensor.shape[0];
-    const height = gray_tensor.shape[1];
-    const num_of_elements = width * height;
-    
-    let gray_tensor_1D = tf.reshape(gray_tensor, [num_of_elements])
-    let gray_tensor_2D = tf.stack(Array(8).fill(gray_tensor_1D), 1);
-    
-    let centroid_dict = {0: [], 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[]};
-    let centroid_dict_res = {0: [], 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[]};
-
-    for (let iteration = 0; iteration < 2; iteration++) {
-        let centroids_2D = tf.stack(Array(num_of_elements).fill(centroids_1D), 0);
-        let difference = tf.sub(gray_tensor_2D, centroids_2D);
-        let distance = tf.square(difference);
-        let indicesOfMinValues = Array.from(tf.argMin(distance, 1).dataSync());
-        
-        console.log(indicesOfMinValues);
-        
-
-        for (let i = 0; i < num_of_elements; i++) {
-            centroid_dict[indicesOfMinValues[i]].push(gray_tensor_1D[i]);
-            centroid_dict_res[indicesOfMinValues[i]].push(i);
-            
-        }
-        
-        let centroids_1D_new = [0, 0, 0, 0, 0, 0, 0, 0];
-        
-        for (let key in centroid_dict) {
-            console.log(centroid_dict[key].length);
-            if (centroid_dict[key].length > 0) {
-                 centroids_1D_new[key] = tf.mean(centroid_dict[key])
-            }  
-        }
-        
-        centroids_1D = centroids_1D_new
-    }
-    
-    
-    let result = new Array(width * height).fill(0);
-        
-
-    Object.keys(centroid_dict_res).forEach(key => {
-        centroid_dict_res[key].forEach(element => {
-            result[element] = parseInt(key); 
-        });
-    });
-            
-        
-    const charWeights = {
-        0: '-',
-        1: '=',
-        2: ':',
-        3: '+',
-        4: '*',
-        5: '%',
-        6: '#',
-        7: '@'
-    };
-    
-    
-    let final_string = ""
-    
-    for (let i = 0; i < result.length; i++) {
-        final_string += charWeights[result[i]];
-        if ((i+1)%100 == 0) {
-            final_string+='\n';
-        }
-    }
-        
-
-    console.log(final_string);
-        
-    document.getElementById('displayArea').innerText = final_string;
-            
-});
-
-
-function reshapeTensor(tensor) {
-    const height = tensor.shape[0];
-    const width = tensor.shape[1];
-    const reshapedTensor =  tensor.reshape([width, height, 3]);
-
-        return reshapedTensor
-};
-
-function reshapeTensor2(tensor) {
-    const height = tensor.shape[1];
-    const width = tensor.shape[0];
-    const reshapedTensor =  tensor.reshape([width, height]);
+    reshapedTensor = tensor.reshape([tensor.shape[1], tensor.shape[0], 3]);
 
     return reshapedTensor
-};
-
-function rgbToGrayscale(imageTensor, height, width) {
-    console.log(imageTensor.shape)
-    const [red, green, blue] = tf.split(imageTensor, 3, 2);
-    console.log(red.shape, green.shape, blue.shape);
-
-    const R_WEIGHT = 0.299;
-    const G_WEIGHT = 0.587;
-    const B_WEIGHT = 0.114;
-
-    // Compute the grayscale values
-    const grayscale = red.mul(R_WEIGHT)
-                         .add(green.mul(G_WEIGHT))
-                         .add(blue.mul(B_WEIGHT))
-//                        
-    
-    return reshapeTensor2(grayscale);
-};
-
-
-
-
+}
